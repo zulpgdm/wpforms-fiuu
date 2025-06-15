f451f98ebc5badc7cf60453f33afd89cf79f5fa0
zulpgdm <zulhelmi@pgdm.my>
2025-06-15 17:11:30 +0800

    Update wpforms-fiuu.php

--- wpforms-fiuu.php
+++ wpforms-fiuu.php
@@ -1,13 +1,30 @@
 <?php
 /**
- * Plugin Name: WPForms Fiuu Gateway
- * Description: Adds Fiuu payment gateway support to WPForms.
- * Version: 1.0.0
- * Author: Zulhelmi
+ * Plugin Name: WPForms FIUU Integration
+ * Description: Adds FIUU payment integration to WPForms.
+ * Version: 1.0
+ * Author: ZulPGDM
  */
 
 defined('ABSPATH') || exit;
 
-require_once plugin_dir_path(__FILE__) . 'src/Loader.php';
+// Autoload plugin classes
+spl_autoload_register(function ($class) {
+    if (strpos($class, 'WPFormsFiuu\\') !== 0) return;
 
-add_action('plugins_loaded', ['WPFormsFiuu\\Loader', 'init']);
+    $class_path = str_replace(['WPFormsFiuu\\', '\\'], ['', '/'], $class);
+    $file = plugin_dir_path(__FILE__) . 'src/' . $class_path . '.php';
+
+    if (file_exists($file)) {
+        require_once $file;
+    }
+});
+
+// Initialize plugin components
+add_action('plugins_loaded', function () {
+    if (!function_exists('wpforms')) return;
+
+    \WPFormsFiuu\Admin\Settings::init();
+    \WPFormsFiuu\Frontend\SubmissionHandler::init();
+    \WPFormsFiuu\Frontend\ReturnHandler::init();
+});
