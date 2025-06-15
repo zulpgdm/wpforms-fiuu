4efe4c190567258fbfae69abe3306c8f8b621455
zulpgdm <zulhelmi@pgdm.my>
2025-06-15 16:25:21 +0800

    Update wpforms-fiuu.php

--- wpforms-fiuu.php
+++ wpforms-fiuu.php
@@ -11,24 +11,3 @@ defined('ABSPATH') || exit;
 require_once plugin_dir_path(__FILE__) . 'src/Loader.php';
 
 add_action('plugins_loaded', ['WPFormsFiuu\\Loader', 'init']);
-
-// src/Loader.php
-<?php
-namespace WPFormsFiuu;
-
-defined('ABSPATH') || exit;
-
-class Loader {
-    public static function init() {
-        require_once __DIR__ . '/Admin/Settings.php';
-        require_once __DIR__ . '/Admin/FormBuilder.php';
-        require_once __DIR__ . '/Frontend/Process.php';
-        require_once __DIR__ . '/Frontend/Callback.php';
-        require_once __DIR__ . '/Helpers/SessionManager.php';
-
-        Admin\Settings::init();
-        Admin\FormBuilder::init();
-        Frontend\Process::init();
-        Frontend\Callback::init();
-    }
-}
